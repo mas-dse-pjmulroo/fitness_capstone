ea7445729a655e1ea56772f98f382717566fc84c
Patrick Mulrooney <mulroony@gmail.com>
2017-04-12 09:24:54 -0700

    add scripts

---
+++ add_fields/correct_time.sql
@@ -0,0 +1,185 @@
+alter table aerobics add column timef integer;
+alter table american_football add column timef integer;
+alter table badminton add column timef integer;
+alter table baseball add column timef integer;
+alter table basketball add column timef integer;
+alter table beach_volleyball add column timef integer;
+alter table bike add column timef integer;
+alter table bike_transport add column timef integer;
+alter table boxing add column timef integer;
+alter table circuit_training add column timef integer;
+alter table climbing add column timef integer;
+alter table core_stability_training add column timef integer;
+alter table cricket add column timef integer;
+alter table cross_country_skiing add column timef integer;
+alter table dancing add column timef integer;
+alter table downhill_skiing add column timef integer;
+alter table elliptical add column timef integer;
+alter table fencing add column timef integer;
+alter table fitness_walking add column timef integer;
+alter table golf add column timef integer;
+alter table gymnastics add column timef integer;
+alter table handball add column timef integer;
+alter table hiking add column timef integer;
+alter table hockey add column timef integer;
+alter table horseback_riding add column timef integer;
+alter table indoor_cycling add column timef integer;
+alter table kayaking add column timef integer;
+alter table kite_surfing add column timef integer;
+alter table martial_arts add column timef integer;
+alter table mountain_bike add column timef integer;
+alter table orienteering add column timef integer;
+alter table pilates add column timef integer;
+alter table polo add column timef integer;
+alter table roller_skiing add column timef integer;
+alter table rowing add column timef integer;
+alter table rugby add column timef integer;
+alter table run add column timef integer;
+alter table sailing add column timef integer;
+alter table scuba_diving add column timef integer;
+alter table skate add column timef integer;
+alter table skateboarding add column timef integer;
+alter table snowboarding add column timef integer;
+alter table snowshoeing add column timef integer;
+alter table soccer add column timef integer;
+alter table squash add column timef integer;
+alter table stair_climing add column timef integer;
+alter table step_counter add column timef integer;
+alter table surfing add column timef integer;
+alter table swimming add column timef integer;
+alter table table_tennis add column timef integer;
+alter table tennis add column timef integer;
+alter table treadmill_running add column timef integer;
+alter table treadmill_walking add column timef integer;
+alter table volleyball add column timef integer;
+alter table walk add column timef integer;
+alter table walk_transport add column timef integer;
+alter table weight_lifting add column timef integer;
+alter table weight_training add column timef integer;
+alter table wheelchair add column timef integer;
+alter table windsurfing add column timef integer;
+alter table yoga add column timef integer;
+#
+#
+#
+update aerobics set timef = time - 38400;
+#UPDATE 700727
+update american_football set timef = time - 38400;
+#UPDATE 107209
+update badminton set timef = time - 38400;
+#UPDATE 168483
+update baseball set timef = time - 38400;
+#UPDATE 18855
+update basketball set timef = time - 38400;
+#UPDATE 71589
+update beach_volleyball set timef = time - 38400;
+#UPDATE 7232
+update bike set timef = time - 38400;
+#UPDATE 111518204
+update bike_transport set timef = time - 38400;
+#UPDATE 33885018
+update boxing set timef = time - 38400;
+#UPDATE 176767
+update circuit_training set timef = time - 38400;
+#UPDATE 1938519
+update climbing set timef = time - 38400;
+#UPDATE 33946
+update core_stability_training set timef = time - 38400;
+#UPDATE 2168159
+update cricket set timef = time - 38400;
+#UPDATE 988
+update cross_country_skiing set timef = time - 38400;
+#UPDATE 1601358
+update dancing set timef = time - 38400;
+#UPDATE 99759
+update downhill_skiing set timef = time - 38400;
+#UPDATE 612909
+update elliptical set timef = time - 38400;
+#UPDATE 1360017
+update fencing set timef = time - 38400;
+#UPDATE 120
+update fitness_walking set timef = time - 38400;
+#UPDATE 1267963
+update golf set timef = time - 38400;
+#UPDATE 168504
+update gymnastics set timef = time - 38400;
+#UPDATE 284372
+update handball set timef = time - 38400;
+#UPDATE 2324
+update hiking set timef = time - 38400;
+#UPDATE 2583390
+update hockey set timef = time - 38400;
+#UPDATE 15060
+update horseback_riding set timef = time - 38400;
+#UPDATE 249169
+update indoor_cycling set timef = time - 38400;
+update kayaking set timef = time - 38400;
+#UPDATE 531068
+update kite_surfing set timef = time - 38400;
+#UPDATE 23019
+update martial_arts set timef = time - 38400;
+#UPDATE 197918
+update mountain_bike set timef = time - 38400;
+#UPDATE 18689995
+update orienteering set timef = time - 38400;
+#UPDATE 1135454
+update pilates set timef = time - 38400;
+#UPDATE 202988
+update polo set timef = time - 38400;
+#UPDATE 1019
+update roller_skiing set timef = time - 38400;
+#UPDATE 730646
+update rowing set timef = time - 38400;
+#UPDATE 696205
+update rugby set timef = time - 38400;
+#UPDATE 3617
+update run set timef = time - 38400;
+#UPDATE 140057634
+update sailing set timef = time - 38400;
+#UPDATE 107292
+update scuba_diving set timef = time - 38400;
+#UPDATE 4894
+update skate set timef = time - 38400;
+#UPDATE 1809964
+update skateboarding set timef = time - 38400;
+#UPDATE 97409
+update snowboarding set timef = time - 38400;
+#UPDATE 97948
+update snowshoeing set timef = time - 38400;
+#UPDATE 47368
+update soccer set timef = time - 38400;
+#UPDATE 202361
+update squash set timef = time - 38400;
+#UPDATE 63854
+update stair_climing set timef = time - 38400;
+#UPDATE 209398
+update step_counter set timef = time - 38400;
+#UPDATE 158610
+update surfing set timef = time - 38400;
+#UPDATE 102421
+update swimming set timef = time - 38400;
+#UPDATE 1020717
+update table_tennis set timef = time - 38400;
+#UPDATE 12941
+update tennis set timef = time - 38400;
+#UPDATE 69529
+update treadmill_running set timef = time - 38400;
+#UPDATE 1602481
+update treadmill_walking set timef = time - 38400;
+#UPDATE 207539
+update volleyball set timef = time - 38400;
+#UPDATE 6562
+update walk set timef = time - 38400;
+#UPDATE 27687925
+update walk_transport set timef = time - 38400;
+#UPDATE 991
+update weight_lifting set timef = time - 38400;
+#UPDATE 500
+update weight_training set timef = time - 38400;
+#UPDATE 4993649
+update wheelchair set timef = time - 38400;
+#UPDATE 4296
+update windsurfing set timef = time - 38400;
+#UPDATE 71027
+update yoga set timef = time - 38400;
+#UPDATE 723745
